f311e89ae80ba7c2e8430d27ed26a767326058d8
Keith Broughton <keithbro256@gmail.com>
2020-03-18 09:47:20 +1100

    Extract runner and image lib

--- index.js
+++ index.js
@@ -1,11 +1,1 @@
-const sharp = require('sharp');
-
-const [filename] = process.argv.slice(2);
-if (!filename) { console.log("A filename is required.") }
-
-sharp(filename)
-  .resize(300, 200)
-  .toFile('output.jpg', function(err) {
-    // output.jpg is a 300 pixels wide and 200 pixels high image
-    // containing a scaled and cropped version of input.jpg
-  });
+require('./lib/egyptian')();
