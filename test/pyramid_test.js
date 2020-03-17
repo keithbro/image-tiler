a28a2d585ced261176d55b0aba96e26b17e62039
Keith Broughton <keithbro256@gmail.com>
2020-03-18 10:10:58 +1100

    Write failing test:

--- test/pyramid_test.js
+++ test/pyramid_test.js
@@ -1,7 +1,15 @@
 const { expect } = require('chai');
+const { levels } = require('../lib/pyramid');
 
-describe('x', () => {
-  it('y', () => {
-    expect(1).to.equal(1)
+describe('.levels', () => {
+  it('returns an array of the levels for an image', () => {
+    const image = { width: 1000, height: 1000 };
+
+    expect(levels(image)).to.equal([
+      { width: 1000, height: 1000 },
+      { width: 500, height: 250 },
+      { width: 500, height: 250 },
+      { width: 500, height: 250 },
+    ]);
   });
 });
