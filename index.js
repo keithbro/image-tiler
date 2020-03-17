d87224f0d8b9176aa0740c5baca6d5aa90efcf05
Keith Broughton <keithbro256@gmail.com>
2020-03-18 09:32:56 +1100

    Take sharp img lib for a test run

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
+const sharp = require('sharp');
 
+const [filename] = process.argv.slice(2);
+if (!filename) { console.log("A filename is required.") }
 
-// Parse command line args
-console.log(process.argv)
+sharp(filename)
+  .resize(300, 200)
+  .toFile('output.jpg', function(err) {
+    // output.jpg is a 300 pixels wide and 200 pixels high image
+    // containing a scaled and cropped version of input.jpg
+  });
