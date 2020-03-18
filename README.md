b8d18949561e9d2ffb8c33020e367015f4e525e2
Keith Broughton <keithbro256@gmail.com>
2020-03-18 18:28:29 +1100

    Move error handling to entrypoint js

--- README.md
+++ README.md
@@ -2,12 +2,18 @@
 
 ## Quick Start
 
+Add an image of your chosing to the root directory of the application e.g. Cat.jpg.
+
 ````
 git clone https://github.com/keithbro/image-tiler.git
 cd image-tiler
 npm install
 npm start Cat.jpg
 ````
+
+## Prerequisites
+
+Node v12 or higher.
 
 ## Tests
 
--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 /**
  * This is the entry point for the CLI tool. All we do is
- * grab Node's filesystem module and pass it through.
+ * grab Node's filesystem module and pass it through, and
+ * handle an overall application error.
  */
 
 const fs = require('fs').promises;
-require('./lib/app')({ fs });
+
+require('./lib/app')({ fs })
+  .catch((e) => {
+    console.error(e.message);
+    process.exit(1);
+  });
